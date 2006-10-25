7fbdef4844894b3e483863fb01d178c71590f13b
aefimov.box <aefimov.box@1a844c48-401d-0410-96c1-69ea7aaf0d43>
2006-10-25 08:25:08 +0000

    IDEADEV-10447, OutOfMemory fix

--- trunk/problemsView/src/org/intellij/trinkets/problemsView/inspections/projectSettings/libraries/LibraryInspection.java
+++ trunk/problemsView/src/org/intellij/trinkets/problemsView/inspections/projectSettings/libraries/LibraryInspection.java
@@ -4,8 +4,8 @@ import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.LibraryOrderEntry;
-import com.intellij.openapi.roots.ModifiableRootModel;
 import com.intellij.openapi.roots.ModuleRootManager;
+import com.intellij.openapi.roots.OrderEntry;
 import com.intellij.openapi.roots.OrderRootType;
 import com.intellij.openapi.roots.libraries.Library;
 import com.intellij.openapi.roots.libraries.LibraryTable;
@@ -19,6 +19,7 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Library inspection.
@@ -26,72 +27,91 @@ import java.util.List;
  * @author Alexey Efimov
  */
 public class LibraryInspection implements ProblemInspection {
-    private final Project project;
+    private static final String APPLICAION_SERVER_LIBRARY_ROOT = "Applicaion server library";
+    private static final String PROJECT_LIBRARY_ROOT = "Project library";
+    private static final String GLOBAL_LIBRARY_ROOT = "Global library";
+    private static final String MODULE_LIBRARY_ROOT = "Module library";
     @NonNls
     private static final String APPLICATION_SERVER_LIBRARIES = "application_server_libraries";
 
+    private final Project project;
+
     public LibraryInspection(Project project) {
         this.project = project;
+    }
+
+    public static void getLibraries(Module module, Set<Library> libs) {
+        OrderEntry[] orderEntries = ModuleRootManager.getInstance(module).getOrderEntries();
+        for (OrderEntry orderEntry : orderEntries) {
+            if (orderEntry instanceof LibraryOrderEntry) {
+                LibraryOrderEntry libEntry = (LibraryOrderEntry) orderEntry;
+                Library lib = libEntry.getLibrary();
+                if (lib != null) {
+                    libs.add(lib);
+                }
+            }
+        }
     }
 
     @NotNull
     public Problem[] inspect() {
         List<Problem> problems = new ArrayList<Problem>();
         LibraryTablesRegistrar libraryTablesRegistrar = LibraryTablesRegistrar.getInstance();
-        checkTable(libraryTablesRegistrar.getLibraryTable(), problems, "Global library");
-        checkTable(libraryTablesRegistrar.getLibraryTable(project), problems, "Project library");
-        checkTable(libraryTablesRegistrar.getLibraryTableByLevel(APPLICATION_SERVER_LIBRARIES, project), problems, "Applicaion server library");
+        checkLibraryTable(GLOBAL_LIBRARY_ROOT, libraryTablesRegistrar.getLibraryTable(), problems);
+        checkLibraryTable(PROJECT_LIBRARY_ROOT, libraryTablesRegistrar.getLibraryTable(project), problems);
+        checkLibraryTable(APPLICAION_SERVER_LIBRARY_ROOT, libraryTablesRegistrar.getLibraryTableByLevel(APPLICATION_SERVER_LIBRARIES, project), problems);
 
         ModuleManager moduleManager = ModuleManager.getInstance(project);
         Module[] modules = moduleManager.getModules();
         for (Module module : modules) {
-            ModuleRootManager moduleRootManager = ModuleRootManager.getInstance(module);
-            ModifiableRootModel model = moduleRootManager.getModifiableModel();
-            LibraryTable table = model.getModuleLibraryTable();
-            Library[] libraries = table.getLibraries();
-            for (Library library : libraries) {
-                List<OrderRootType> types = new ArrayList<OrderRootType>(0);
-                if (check(library, OrderRootType.CLASSES)) {
-                    types.add(OrderRootType.CLASSES);
-                }
-                if (check(library, OrderRootType.SOURCES)) {
-                    types.add(OrderRootType.SOURCES);
-                }
-                if (check(library, OrderRootType.JAVADOC)) {
-                    types.add(OrderRootType.JAVADOC);
-                }
-                if (types.size() > 0) {
-                    LibraryOrderEntry libraryOrderEntry = model.findLibraryOrderEntry(library);
-                    if (libraryOrderEntry != null) {
-                        problems.add(new LibraryProblem("Module library", ProjectRootConfigurable.trancateModuleLibraryName(libraryOrderEntry), types));
-                    }
-                }
-            }
+            checkModuleLibraries(module, problems);
         }
 
         return problems.toArray(Problem.EMPTY_PROBLEM_ARRAY);
     }
 
-    private static void checkTable(LibraryTable table, List<Problem> problems, String rootName) {
+    private static void checkLibraryTable(String rootName, LibraryTable table, List<Problem> problems) {
         Library[] libraries = table.getLibraries();
         for (Library library : libraries) {
-            List<OrderRootType> types = new ArrayList<OrderRootType>(0);
-            if (check(library, OrderRootType.CLASSES)) {
-                types.add(OrderRootType.CLASSES);
-            }
-            if (check(library, OrderRootType.SOURCES)) {
-                types.add(OrderRootType.SOURCES);
-            }
-            if (check(library, OrderRootType.JAVADOC)) {
-                types.add(OrderRootType.JAVADOC);
-            }
-            if (types.size() > 0) {
-                problems.add(new LibraryProblem(rootName, library.getName(), types));
+            checkLibrary(rootName, library.getName(), library, problems);
+        }
+    }
+
+    private static void checkModuleLibraries(Module module, List<Problem> problems) {
+        ModuleRootManager moduleRootManager = ModuleRootManager.getInstance(module);
+        OrderEntry[] orderEntries = moduleRootManager.getOrderEntries();
+        for (OrderEntry entry : orderEntries) {
+            if (entry instanceof LibraryOrderEntry) {
+                LibraryOrderEntry libraryOrderEntry = (LibraryOrderEntry) entry;
+                Library library = libraryOrderEntry.getLibrary();
+                if (library != null) {
+                    String libName = ProjectRootConfigurable.trancateModuleLibraryName(libraryOrderEntry);
+                    checkLibrary(MODULE_LIBRARY_ROOT, libName, library, problems);
+                }
             }
         }
     }
 
-    private static boolean check(Library library, OrderRootType rootType) {
+    private static void checkLibrary(String rootName, String libraryName, Library library, List<Problem> problems) {
+        boolean wrongClasses = checkLibraryRoot(library, OrderRootType.CLASSES);
+        boolean wrongSources = checkLibraryRoot(library, OrderRootType.SOURCES);
+        boolean wrongJavaDoc = checkLibraryRoot(library, OrderRootType.JAVADOC);
+        if (wrongClasses || wrongSources || wrongJavaDoc) {
+            List<OrderRootType> types = new ArrayList<OrderRootType>(3);
+            if (wrongClasses) {
+                types.add(OrderRootType.CLASSES);
+            }
+            if (wrongSources) {
+                types.add(OrderRootType.SOURCES);
+            }
+            if (wrongJavaDoc) {
+                types.add(OrderRootType.JAVADOC);
+            }
+            problems.add(new LibraryProblem(rootName, libraryName, types));
+        }
+    }
+
+    private static boolean checkLibraryRoot(Library library, OrderRootType rootType) {
         String[] urls = library.getUrls(rootType);
         for (String url : urls) {
             if (!FindFileUtil.isValidUrl(url)) {
