2a6d28923ab285c78e8be818c0775022f36cd821
aefimov.box <aefimov.box@1a844c48-401d-0410-96c1-69ea7aaf0d43>
2007-03-16 12:31:42 +0000

    Fix rendering images

--- trunk/win32/contextMenu/cpp/ContextMenuHelper.h
+++ trunk/win32/contextMenu/cpp/ContextMenuHelper.h
@@ -29,7 +29,11 @@ public:
     IContextMenu *m_lpcm;
     HDC m_hDC;
 
-    HMENU FindMenuItem(HMENU hMenu, UINT wID);
+    HRESULT QueryContextMenu(HMENU hMenu);
+	HRESULT GetCommandString(HMENU hMenu, UINT index, char* szHelpText);
+	HRESULT InvokeCommand(HMENU hMenu, HWND hWnd, UINT index);
+
+    HMENU FindMenuItem(HMENU hMenu, UINT index);
     HBITMAP InitializeOwnerDrawItem(HMENU hMenu, UINT index, SIZE *lpSize);
     HBITMAP InitializeCheckMarkItem(HMENU hMenu, UINT index);
     CContextMenuHelper(HWND hWnd, std::vector<LPCTSTR> files);
@@ -37,20 +41,20 @@ public:
 
 private:
     HWND m_hWnd;
-   	int m_iLeftOfText;
-	LPSTR m_pBuffer;
+       int m_iLeftOfText;
+    LPSTR m_pBuffer;
 
     void FreeItemIDList(std::vector<LPITEMIDLIST> &v, IMalloc *pm);
     LPITEMIDLIST CopyPIDL(LPCITEMIDLIST pidl, IMalloc *pm);
     HRESULT SHGetContextMenu(std::vector<LPCTSTR> files);
     HRESULT SHBindToParentEx(LPCITEMIDLIST pidl, REFIID riid, LPVOID *ppv, LPCITEMIDLIST *ppidlLast, IShellFolder *psfDesktop, IMalloc *pm);
-	static int CALLBACK EnhMetaFileProc(
-	  HDC hDC,                      // handle to DC
-	  HANDLETABLE *lpHTable,        // metafile handle table
-	  CONST ENHMETARECORD *lpEMFR,  // metafile record
-	  int nObj,                     // count of objects
-	  LPARAM lpData                 // optional data
-	);
+    static int CALLBACK EnhMetaFileProc(
+      HDC hDC,                      // handle to DC
+      HANDLETABLE *lpHTable,        // metafile handle table
+      CONST ENHMETARECORD *lpEMFR,  // metafile record
+      int nObj,                     // count of objects
+      LPARAM lpData                 // optional data
+    );
 };
 
 #endif // !defined(AFX_CONTEXTMENUHELPER_H__4D06B1C1_AF4F_4A3D_BB3F_C0093A5F656F__INCLUDED_)
