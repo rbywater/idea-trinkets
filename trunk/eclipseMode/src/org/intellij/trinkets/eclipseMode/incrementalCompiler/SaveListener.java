342a6a14a5c057954a13e527b6ca4afb46398b90
Valerii.Makieienko@gmail.com <Valerii.Makieienko@gmail.com@1a844c48-401d-0410-96c1-69ea7aaf0d43>
2011-09-07 08:34:24 +0000

    Issue 7:	Added two wrappers to satisfy assertions regarding threads calling "isSynchronized" and "buildProject" (java.lang.AssertionError in eclipse mode plugin)

--- trunk/eclipseMode/src/org/intellij/trinkets/eclipseMode/incrementalCompiler/SaveListener.java
+++ trunk/eclipseMode/src/org/intellij/trinkets/eclipseMode/incrementalCompiler/SaveListener.java
@@ -1,17 +1,24 @@
 package org.intellij.trinkets.eclipseMode.incrementalCompiler;
 
-import com.intellij.openapi.compiler.CompileScope;
+import com.intellij.analysis.AnalysisScopeBundle;
+import com.intellij.compiler.impl.ModuleCompileScope;
 import com.intellij.openapi.compiler.CompilerManager;
 import com.intellij.openapi.module.Module;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.progress.ProgressManager;
+import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
+import com.intellij.openapi.project.ProjectUtil;
 import com.intellij.openapi.roots.ProjectFileIndex;
 import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.openapi.vfs.VirtualFileAdapter;
 import com.intellij.openapi.vfs.VirtualFileCopyEvent;
 import com.intellij.openapi.vfs.VirtualFileEvent;
 import com.intellij.openapi.vfs.VirtualFileMoveEvent;
+import com.intellij.util.ui.UIUtil;
 import org.intellij.trinkets.eclipseMode.EclipseMode;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * VFS listener to handle save file event.
@@ -44,24 +51,35 @@ public class SaveListener extends VirtualFileAdapter {
         executeMake(event);
     }
 
-    private void executeMake(VirtualFileEvent event) {
+    private void executeMake(final VirtualFileEvent event) {
+        final Project project = ProjectUtil.guessProjectForFile(event.getFile());
+        ProgressManager.getInstance().run(new Task.Backgroundable(project, AnalysisScopeBundle.message("analyzing.project"), true) {
+            public void run(@NotNull ProgressIndicator indicator) {
+                executeMakeInUIThread(event);
+            }
+        });
+    }
+
+    private void executeMakeInUIThread(VirtualFileEvent event) {
         EclipseMode eclipseMode = EclipseMode.getInstance();
         if (event.isFromSave() && eclipseMode.getSettings().INCREMENTAL_COMPILATION_ENABLED) {
             Project[] projects = ProjectManager.getInstance().getOpenProjects();
             for (final Project project : projects) {
                 if (project.isInitialized() && !project.isDisposed() &&
                         project.isOpen() && !project.isDefault()) {
-                    ProjectFileIndex projectFileIndex = ProjectRootManager.getInstance(project).getFileIndex();
+                    final ProjectFileIndex projectFileIndex = ProjectRootManager.getInstance(project).getFileIndex();
                     final Module module = projectFileIndex.getModuleForFile(event.getFile());
                     if (module != null) {
-                        CompilerManager compilerManager = CompilerManager.getInstance(project);
+                        final CompilerManager compilerManager = CompilerManager.getInstance(project);
                         if (!compilerManager.isCompilationActive() &&
-                                !compilerManager.isExcludedFromCompilation(event.getFile())) {
-                            CompileScope compileScope = compilerManager.createModuleCompileScope(module, false);
-                            if (!compilerManager.isUpToDate(compileScope)) {
-                                // Changed file found in module. Make it.
-                                compilerManager.make(compileScope, null);
-                            }
+                                !compilerManager.isExcludedFromCompilation(event.getFile()) &&
+                                !compilerManager.isUpToDate(new ModuleCompileScope(module, false))) {
+                            // Changed file found in module. Make it.
+                            UIUtil.invokeAndWaitIfNeeded(new Runnable() {
+                                public void run() {
+                                    compilerManager.make(module, null);
+                                }
+                            });
                         }
                     }
                 }
